ce7ea133c8f6439f35f0fa46e5dcd8f5a87a32ea
dariomtz <gusdamtzr@gmail.com>
2023-07-01 14:27:26 -0600

    Set up run command

---
+++ main.py
@@ -0,0 +1,20 @@
+import argparse
+from run import run
+
+
+def main() -> None:
+    # Create parser
+    parser = argparse.ArgumentParser("Interviews")
+    subparsers = parser.add_subparsers()
+
+    # create parser for the program run
+    parser_run = subparsers.add_parser("run", help="run the specified program")
+    parser_run.add_argument("file", type=str, help="specifies the file to run")
+
+    args = parser.parse_args()
+
+    run(args.file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ run.py
@@ -0,0 +1,2 @@
+def run(file: str) -> None:
+    pass
