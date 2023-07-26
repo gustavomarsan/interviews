2fcf00ac7f7d63713e494eb09c5d4b7cbc7dcc9e
gustavomarsan <gustavo.mtz012@gmial.com>
2023-07-25 23:13:17 -0600

    Add program factorial

---
+++ programs/factorial.py
@@ -0,0 +1,15 @@
+# calcular el factorial de un numero
+# funcion
+
+def factorial(n):
+    if n==0 or n==1:
+        resultado=1
+        
+    elif n>1 :
+        resultado=n*factorial(n-1)
+    
+    return resultado
+
+fac= factorial(5)
+print (fac)
+
