7bf5e0c67eb58bae1501e58f95a94d7e3fd298a4
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-12 14:26:10 -0600

    Customize progrm

--- programs/factorial.py
+++ programs/factorial.py
@@ -1,14 +1,13 @@
 # calcular el factorial de un numero
 
-def factorial(n):
+def factorial(n:int)->int:
     if n==0 or n==1:
         return 1
         
     elif n>1 :
        return n*factorial(n-1)
     
-    return resultado
+    return -1
 
-fac= factorial(int(input()))
-print (fac)
+print(factorial(int(input())))
 
