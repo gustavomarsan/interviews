4e8b315b0eb9fd321ff5bcb7fb51bce3a8059711
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-15 20:15:53 -0600

    Simple changes

--- programs/rotate_matrix.py
+++ programs/rotate_matrix.py
@@ -3,25 +3,30 @@
 import os
 
 def rotateMatrix(a):
-    b=a
+    b=[[0,0,0],
+       [0,0,0],
+       [0,0,0]]
     for i in range(len(a)):
         for n in range(len(a)):
             #print(i,n)
-            #b[n][i]=a[i][n]
-            #print(b[n][i])
+            b[n][i]=a[i][n]
+            print("*",b[n][i])
             print(a[i][n])
-
+            
     return b
 
 os.system("clear")
 
-a=[[1,2,3],[4,5,6],[7,8,9]]
+a=[[1,2,3],
+   [4,5,6],
+   [7,8,9]]
 
-b=rotateMatrix(a)
 
 print((a[0]))
 print((a[1]))
 print((a[2]))
+
+b=rotateMatrix(a)
 
 print((b[0]))
 print((b[1]))
