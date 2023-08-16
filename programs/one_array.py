29e700aebaf11fa25b0f45257d5a29c9f406bae8
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-15 22:14:13 -0600

    Address coments

--- programs/one_array.py
+++ programs/one_array.py
@@ -1,48 +1,40 @@
-# debido a que a se pueden resalizar 3 tipos de ediciones a una lista (agregar, modificar o eliminar) 
+# debido a que a se pueden resalizar 3 tipos de ediciones a una lista (agregar, modificar o eliminar)
 # dados 2 strings, comparar si uno puede ser reasultado de un movimiento del otro (solo un movimiento)
 
-import os
 
-
-def onearray(a,b):
-
-    if abs(len(a)-len(b)) >1 :  #comparar arrays para sabe si añadio ó  elimino
+def onearray(a, b):
+    if abs(len(a) - len(b)) > 1:  # comparar arrays para sabe si añadio ó  elimino
         return False
 
-    if len(a)-len(b) == 0 :      #dif es cero, no elimino ni añadio, verificar si hubo un cambio
+    # dif es cero, no elimino ni añadio, verificar si hubo un cambio
+    if len(a) - len(b) == 0:
         error = 0
-        for i in range (len(a)):
-            if  a[i] != b[i] :
+        for i in range(len(a)):
+            if a[i] != b[i]:
                 error += 1
-                print("error number ",error)
-                if error == 2 :
+                print("error number ", error)
+                if error == 2:
                     return False
         return True
-    
-    if len(b) > len (a) :       # dejar el string mas grande en a
-        c=a
-        a=b
-        b=c
+
+    if len(b) > len(a):  # dejar el string mas grande en a
+        c = a
+        a = b
+        b = c
 
     # comaenzar comparacion de string con diferencia de 1 caracter
     error = 0
-    for i in range (len(b)):
-        if error == 0:
-            n=i
-        else :
-            n=i+1
-        
-        if a[n] != b[i] :
+    for i in range(len(b)):
+        n = i if error == 0 else i + 1
+
+        if a[n] != b[i]:
             error += 1
-            print("error number ",error)
-            if error == 2 :
+            print("error number ", error)
+            if error == 2:
                 return False
-    
-    #si llego hasta aqui complio con condiciones true
+
+    # si llego hasta aqui complio con condiciones true
     return True
 
 
-
-os.system("clear")
-print(onearray(input(),input()))
-
+print(onearray(input(), input()))
