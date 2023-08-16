c852cd45bdd0815d123dbfeb9d0da25940149fb1
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-15 22:08:58 -0600

    Format factorial

---
+++ programs/factorial.py
@@ -0,0 +1,14 @@
+# calcular el factorial de un numero
+
+
+def factorial(n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+
+    elif n > 1:
+        return n * factorial(n - 1)
+
+    return -1
+
+
+print(factorial(int(input())))
