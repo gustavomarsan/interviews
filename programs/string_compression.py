0bbcf7c947208e9081a94d2a52f9b424dfaa67fb
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-16 21:09:40 -0600

    Finished

--- programs/string_compression.py
+++ programs/string_compression.py
@@ -1,29 +1,32 @@
 # implement a method to perform a basic string compression using the counts of repetead characters
 # if the compressed string would not become smaller, the method should return the original string
+# do not be concatenating strings repeatly
 
-import os
 
 def stringCompression(a):
-    last_char=a[0]
-    cont=1
-    b=""
-    for i in range (len(a)):
-        print(last_char)
-        new_char=a[i]
-        if new_char == last_char :
-            cont+=1
-        else:
-            b+=last_char+str(cont)
-            last_char=new_char
-            cont=1
-    
-    b+=last_char+str(cont)
-    if len(b) < len(a):
-        return b
-    else :
+    last_char = a[0]
+    cont = 0
+    b = []
+    for i in range(len(a)):
+        new_char = a[i]
+        if new_char == last_char:
+            cont += 1
+        else:  # add info to list when char changes
+            b.append(last_char)
+            b.append(str(cont))
+            last_char = new_char
+            cont = 1
+
+    # add the last info
+    b.append(last_char)
+    b.append(str(cont))
+
+    bstring = "".join(b)
+
+    if len(bstring) < len(a):
+        return bstring
+    else:
         return "No change"
 
-
-os.system("clear")
 
 print(stringCompression(input()))
