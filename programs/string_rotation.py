fafd8b2daa88b60a354521533171bb52ed707b3d
gustavomarsan <gustavo.mtz012@gmial.com>
2023-08-23 23:00:51 -0600

    Neither delete or add elements from de list

--- programs/string_rotation.py
+++ programs/string_rotation.py
@@ -14,8 +14,10 @@ def string_rotation(s1, s2):
         l2.append(i)
 
     for i in range(len(l2) - 1):  # rotate l1 char by char
-        l2.append(l2[0])
-        l2.pop(0)
+        temp = l2[0]
+        for n in range(len(l2) - 1):  # rotate l1 char by char
+            l2[n] = l2[n + 1]
+        l2[-1] = temp
         if l2 == l1:
             return True
 
