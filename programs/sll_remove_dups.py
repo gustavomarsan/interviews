d5f860b28b9877e39d7b81a5863f3d9eb66ecc34
gustavomarsan <gustavo.mtz012@gmial.com>
2023-09-05 22:21:01 -0600

    Final changes

--- programs/sll_remove_dups.py
+++ programs/sll_remove_dups.py
@@ -2,10 +2,6 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
-
-class SingleLinkedList:
-    def __init__(self):
         self.head = None
 
     def display(self):
@@ -14,7 +10,9 @@ class SingleLinkedList:
         else:
             temp = self.head
             while temp:
-                print(temp.data, " -->", end="  ")
+                print(temp.data, end="  ")
+                if temp.next:
+                    print("--->", end=" ")
                 temp = temp.next
             print()
 
@@ -25,41 +23,26 @@ class SingleLinkedList:
             while p2.next:
                 if p1.data == p2.next.data:
                     if p2.next.next == None:
-                        p2.next = None
+                        p2.next = None  # p2
                         break
                     else:
                         p2.next = p2.next.next
                         continue
                 p2 = p2.next
 
-            p1 = p1.next
+            # Maybe when loop while p1.next began, p1.next had some value and after change to None. Validate
+            if p1.next != None:
+                p1 = p1.next
 
 
-# Create a single linked list
-L = SingleLinkedList()
-n0 = Node(1)
-L.head = n0
-n1 = Node(5)
-n0.next = n1
-n2 = Node(1)
-n1.next = n2
-n3 = Node(9)
-n2.next = n3
-n4 = Node(2)
-n3.next = n4
-n5 = Node(3)
-n4.next = n5
-n6 = Node(7)
-n5.next = n6
-n7 = Node(8)
-n6.next = n7
-n8 = Node(14)
-n7.next = n8
-n9 = Node(15)
-n8.next = n9
+n = Node(input("Head: "))
+h = n
+h.head = n
+for i in range(int(input("How many extra nodes? "))):
+    nn = Node(input("Next node: "))
+    n.next = nn
+    n = n.next
 
-print("Original linked list")
-L.display()
-
-L.RemDups()
-L.display()
+h.display()
+h.RemDups()
+h.display()
