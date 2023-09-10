f00d57ba798b1e799ca1563b242a7c4c2a38a8ca
gustavomarsan <gustavo.mtz012@gmial.com>
2023-09-09 21:26:53 -0600

    String rotation done in O(N)

--- programs/string_rotation.py
+++ programs/string_rotation.py
@@ -1,4 +1,4 @@
-# Given 2 strings check if s2 is a rotation of s1 using one call
+# Given 2 strings check if s2 is a rotation of s1 using one call, supouse the 2 strings are the same large
 
 
 def string_rotation(s1, s2):
@@ -7,25 +7,28 @@ def string_rotation(s1, s2):
 
     # put the strings in lists to work in lists not in strings
     l1 = []
-    for i in s1:
-        l1.append(i)
     l2 = []
-    for i in s2:
-        l2.append(i)
+    for i in range(len(s1)):
+        l1.append(s1[i])
+        l2.append(s2[i])
 
-    for i in range(len(l2) - 1):  # rotate l1 char by char
-        temp = l2[0]
-        for n in range(len(l2) - 1):  # rotate l1 char by char
-            l2[n] = l2[n + 1]
-        l2[-1] = temp
-        if l2 == l1:
+    for i in range(len(l2) - 1):
+        l2.append(
+            l2[0]
+        )  # rotate l2 in 2 setps, copy [0] at the end and delete it from de begining
+        l2.pop(0)
+        if l2 == l1:  # compare rotated l2 with l1
             return True
 
     # if the program never match
     return False
 
-    # if s1 == s2 :
-    #    return True
-
 
 print(string_rotation(input(), input()))
+
+#    l1 = []
+#    for i in s1:
+#        l1.append(i)
+#    l2 = []
+#    for i in s2:
+#        l2.append(i)
