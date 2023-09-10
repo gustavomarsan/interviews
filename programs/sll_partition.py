49766824eed46f612d6dcf553371e373d0ccfc92
gustavomarsan <gustavo.mtz012@gmial.com>
2023-09-09 20:15:25 -0600

    Sll partition

---
+++ programs/sll_partition.py
@@ -0,0 +1,58 @@
+# in a sll make a partition to around a value x. All nodes less than x  come before that all nodes equal or greater to x
+# the x value can appear any where in the right partition. x not needs to apear in the middle, just in right partition.
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.head = None
+
+    def display(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, end="  ")
+                if temp.next:
+                    print("--->", end=" ")
+                temp = temp.next
+            print()
+
+
+for i in range(int(input("How many nodes? "))):
+    nn = Node(int(input(f"Node {i} : ")))
+    if i == 0:
+        n = nn
+        h = n
+        h.head = n
+    n.next = nn
+    n = n.next
+
+
+h.display()
+x = int(input("Which element wants to be the partition reference: "))
+
+p1 = h.head  # p1 = pointer 1
+
+while p1.next:  # p1 walk sll
+    if p1.data < x:
+        p1 = p1.next
+        continue
+    p2 = p1.next  # p2 = poniter 1
+    while p2:  # p2 walk sll
+        if (
+            p2.data < p1.data and p2.data < x
+        ):  # compare p1 and p2 and if necesary switch them
+            temp = p1.data
+            p1.data = p2.data
+            p2.data = temp
+            break
+        if p2.next:
+            p2 = p2.next
+        else:
+            break
+    p1 = p1.next
+
+h.display()
