d3e72abef38972e44baf9927ea60de606526e657
gustavomarsan <gustavo.mtz012@gmial.com>
2023-09-22 18:35:22 -0600

    Program solve using new Class Node

--- programs/sll_find_kth_element.py
+++ programs/sll_find_kth_element.py
@@ -2,13 +2,12 @@
 
 from single_linked_lists import Node
 
-h = Node(None)
-h = h.list_builder()
-h.display()
+h = Node.list_builder()
+print(h)
 
 k = int(input("Kth element to find from the last: "))
 
-p1 = h.head  # p1 is ponter 1
+p1 = h  # p1 is ponter 1
 p2 = p1
 
 for n in range(k):  # p2 walks k elements to get diference k vs. p1
