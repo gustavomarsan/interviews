1d9a0960546e6162dbf49a1b69557ca1d3102ae6
gustavomarsan <gustavo.mtz012@gmial.com>
2023-09-27 21:20:41 -0600

    Program finished sending only the node to delete

--- programs/sll_delete_middle_node.py
+++ programs/sll_delete_middle_node.py
@@ -1,6 +1,15 @@
 # in a single linked list delete a node in de middle, not the first, not de last, not necesary the exact middle
+# You only receive the Node to delete, not de sll
+
 
 from single_linked_lists import Node
+
+
+# I have not element before I want to delete, I will replace values with next values  and delete next
+def delete_element(element: Node):
+    print("Hola")
+    element.data = element.next.data
+    element.next = element.next.next
 
 
 h = Node.list_builder()
@@ -10,18 +19,19 @@ k = int(input("Which element in the middle do You want to delete?: "))
 
 found = 0
 n = h
-while n.next:
-    # this if checks is next.data element is equal to elenment to delete and
+# Search element to delete
+while n and n.next:
+    # this if checks is next.data element is equal to element to delete and
     # n.next.next  validation is because the last element can´t be deleted
     if n.next.data == k and n.next.next:
-        n.next = n.next.next
+        element = n.next
         found = 1
         break  # found the element to delete and break
     n = n.next
 
 if found == 0:
     print("element not found in de middle")
-else:
-    print("element found and deleted")
 
-print(h)
+else:
+    delete_element(element)
+    print(h)
