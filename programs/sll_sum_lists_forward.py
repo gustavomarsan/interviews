d25e2bc7e282e843b006c7742b232091fcaf6b8c
gustavomarsan <gustavo.mtz012@gmial.com>
2023-10-11 21:20:00 -0600

    Solve in less lines  ; )

--- programs/sll_sum_lists_forward.py
+++ programs/sll_sum_lists_forward.py
@@ -5,62 +5,49 @@
 from single_linked_lists import Node
 
 
+def sum_one_ten(a: Node, b: Node, result: Node):
+    if a.next:
+        sum_one_ten(a.next, b.next, result)
+
+    new_node = Node(a.data + b.data + result.data)
+    temp = result.next
+    result.next = new_node
+    new_node.next = temp
+    if result.next.data > 9:  # manage the ten in result.head
+        result.next.data -= 10
+        result.data = 1
+    else:
+        result.data = 0
+        return result.next
+    return result
+
+
 def sum_nodes(a: Node, b: Node):
     head_a = a
     head_b = b
-    len_a = 0
-    len_b = 0
 
-    # compare len of the 2 sll
-    while a or b:
+    while a or b:  # fill 0s in smaller sll
         if a:
             a = a.next
-            len_a += 1
-        if b:
-            b = b.next
-            len_b += 1
-
-    # add 0 to the smaller sll
-    if (len_a - len_b) < 0:
-        for n in range(len_b - len_a):
+        else:
             temp = head_a
             head_a = Node(0)
             head_a.next = temp
 
-    if (len_a - len_b) > 0:
-        for n in range(len_a - len_b):
+        if b:
+            b = b.next
+        else:
             temp = head_b
             head_b = Node(0)
             head_b.next = temp
 
-    a = head_a
-    b = head_b
-
-    print("Fill 0s")
-    print(a)
-    print(b)
-
     head = Node(0)
-    resul = head
-    ten = 0
-    while a or ten:
-        sum = a.data + b.data + ten
-        ten = 0
-        if sum > 9:
-            sum -= 10
-            ten = 1
-        new_node = Node(sum)
-        resul.next = new_node
-        resul = resul.next
-        a = a.next
-        b = b.next
+    result = head
 
-    return head.next
+    return sum_one_ten(head_a, head_b, result)
 
 
 a = Node.list_builder()
 b = Node.list_builder()
 
-print(a)
-print(b)
 print(sum_nodes(a, b))
