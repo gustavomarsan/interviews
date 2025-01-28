82e874c0dc1f1cfe50defb938df58ebee0170231
gustavomarsan <105552247+gustavomarsan@users.noreply.github.com>
2025-01-28 17:57:00 -0600

    practicing (#52)
    Co-authored-by: gustavomarsan <gustavo.mtz012@gmial.com>

---
+++ programs/maximum_neighbor_diff.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/
+3423. Maximum Difference Between Adjacent Elements in a Circular Array
+Given a circular array nums, find the maximum absolute difference between adjacent elements.
+Note: In a circular array, the first and last elements are adjacent.
+Example 1:
+Input: nums = [1,2,4]
+Output: 3
+Explanation:
+Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+Example 2:
+Input: nums = [-5,-10,-5]
+Output: 5
+Explanation:
+The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+Constraints:
+2 <= nums.length <= 100
+-100 <= nums[i] <= 100
+
+"""
+
+def max_neighbor_diff(nums: list[int])-> int :
+    max_diff = -float('inf')
+    for i in range(len(nums) - 1) :
+        max_diff = max(max_diff, abs(nums[i + 1] - nums[i]))
+    return max(max_diff, abs(nums[-1] - nums[0]))
+    
+
+nums = [1, 2, 4]
+print(max_neighbor_diff(nums))
